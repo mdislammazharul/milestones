25a9c91e88c0416a8809c4615bfc86c16d16dc18
mdislammazharul <mazharul.py@gmail.com>
2021-09-03 17:22:47 +0600

    scope

---
+++ Milestone-6/Checklist/scope.js
@@ -0,0 +1,3 @@
+// const & let creats a BLOCK scope. that means the variable can be accessed only from the block where it created. but not outside from the block.
+
+// if we use var insted of const / let, then it (var) makes a hoisting for itself. that means, we cannot access the value of the var, but program will not show any error. it shows only UNDEFINED for the var variable.
