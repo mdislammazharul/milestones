36c8b64665bf9c0d182a857dbea79eb94e06810e
mdislammazharul <mazharul.py@gmail.com>
2021-09-05 23:00:58 +0600

    browser

--- Milestone-7/Browser api/alert.js
+++ Milestone-7/Browser api/alert.js
@@ -1,3 +1,25 @@
+// alert --> only ok
 const momComing = () => {
-    alert("I'm here!")
+    alert("I'm here!");
+}
+
+// confirm --> ok & no
+const askPicnic = () => {
+    const response = confirm('Are you going to picnic?');
+    console.log(response);
+    if (response == true) {
+        alert("Give me the money!!")
+    }
+    else {
+        alert("DGM !!");
+    }
+}
+
+// prompt
+const askName = () => {
+    const name = prompt('What is your name');
+    if (name) {
+        console.log(name);
+    }
+
 }
---
+++ Milestone-7/Browser api/reload.js
@@ -0,0 +1,7 @@
+const refreshPage = () => {
+    location.reload();
+}
+
+const facebook = () => {
+    window.location.assign('https://facebook.com');
+}
