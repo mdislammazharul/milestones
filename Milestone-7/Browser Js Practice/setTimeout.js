d33965641173c47cbccbfd613a1d8bd0f701f05d
mdislammazharul <mazharul.py@gmail.com>
2021-09-06 11:16:48 +0600

    setTimeout

---
+++ Milestone-7/Browser Js Practice/setTimeout.js
@@ -0,0 +1,9 @@
+// const timeOutID = setTimeout(
+//     function () {
+//         console.log('Hello World');
+//     }, 3500);
+
+// arrow function
+const timeOutID = setTimeout(() => {
+    console.log('Hello World');
+}, 3500)
