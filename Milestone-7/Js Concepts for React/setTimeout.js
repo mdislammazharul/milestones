4085b313140b2b6c72fb2101cec905fb5a32f2d2
mdislammazharul <mazharul.py@gmail.com>
2021-09-12 10:54:09 +0600

    setInterval

--- Milestone-7/Js Concepts for React/setTimeout.js
+++ Milestone-7/Js Concepts for React/setTimeout.js
@@ -7,3 +7,14 @@ setTimeout(() => {
 setTimeout(() => {
     console.log("I'm faster than the previous one");
 }, 2000)
+
+let i = 0;
+const intervalId = setInterval(() => {
+    console.log("I'm coming!!")
+    if (i > 5) {
+        clearInterval(intervalId);
+    }
+    i++;
+}, 2000);
+
+
