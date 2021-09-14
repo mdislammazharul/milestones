6906726230a148728a70603b85c15410e9123c02
mdislammazharul <mazharul.py@gmail.com>
2021-09-14 15:31:40 +0600

    error text

--- Milestone-7/Js Concepts for React/practice.js
+++ Milestone-7/Js Concepts for React/practice.js
@@ -1,19 +1,3 @@
-var myCar = {
-    make: 'Ford',
-    model: 'Mustang',
-    year: 1969
-};
-console.log(myCar.make);
-function showProps(obj, objName) {
-    var result = ``;
-    for (var i in obj) {
-        // obj.hasOwnProperty() is used to filter out properties from the object's prototype chain
-        if (obj.hasOwnProperty(i)) {
-            result += `${objName}.${i} = ${obj[i]}\n`; // very important --> obj[i]... obj.i will be wrong. because if we access data from an object by 'Variable' , then we have to use [i]. 
-        }
-    }
-    return result;
-}
+console.log(a)
 
-const car = showProps(myCar, "myCar");
-console.log(car);
+let a = 'mazhar';
