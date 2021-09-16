3beb73f7f3938190eac57f0f437a576f641bf9c1
mdislammazharul <mazharul.py@gmail.com>
2021-09-17 00:25:16 +0600

    destructuring

--- Milestone-8/Simple React/Js/countries.js
+++ Milestone-8/Simple React/Js/countries.js
@@ -10,11 +10,14 @@ const displayCountries = countries => {
     container.innerHTML = countryHTML.join(' ');
 }
 const getCountryHTML = country => {
+    const { name, capital, flag } = country;
+    const { name: currenciesName } = country.currencies[0]; // destructuring
     return `
         <div class="country">
-            <h2>${country.name}</h>
-            <h4>Capital: ${country.capital}</h4>
-            <img src="${country.flag}">
+            <h2>${name}</h2>
+            <h4>Capital: ${capital}</h4>
+            <h4>Currencie: ${currenciesName}</h4>
+            <img src="${flag}">
         </div>`
 }
 loadCountries();
