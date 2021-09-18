521f35eabebe222e98040eab695f4440ca245e93
mdislammazharul <mazharul.py@gmail.com>
2021-09-19 00:20:43 +0600

    api data load

--- Milestone-8/react_recap/src/App.js
+++ Milestone-8/react_recap/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadUsers></LoadUsers>
       <Mycomponent product="laptop" price="50000"></Mycomponent>
       <Mycomponent product="mobile" price="20000"></Mycomponent>
       <Mycomponent product="watch" price="3000"></Mycomponent>
@@ -12,6 +13,44 @@ function App() {
   );
 }
 
+// ----------- Load User Data -------------
+
+function LoadUsers() {
+  // state declaration
+  const [users, setUsers] = useState([])
+  // function for data load --> useEffect
+  // useEffect will take two parameters --> 1. an anonymous function 2. an empty array
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data)) // fetched data have to set in setUsers --> 'setUsers' from state declaration
+  }, [])
+  return (
+    <div>
+      <h1>Users Loaded: {users.length}</h1>
+      {
+        users.map(user => <User name={user.name} phone={user.phone}></User>)
+      }
+    </div>
+  )
+}
+
+// component for Users
+function User(props) {
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      border: '3px solid lightsalmon',
+      margin: '20px',
+      borderRadius: '20px'
+    }}>
+      <h2>Name: {props.name}</h2>
+      <p>Call me: {props.phone}</p>
+    </div>
+  )
+}
+
+// ----------- Add Ponit ---------------
 function Mycomponent(props) {
   const [point, setPoint] = useState(1)
   const myStyle = {
