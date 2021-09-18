9d48b1de8b044436f759dee47523713d6eafafd3
mdislammazharul <mazharul.py@gmail.com>
2021-09-18 17:58:40 +0600

    Api Comment

--- Milestone-8/component_2/src/App.js
+++ Milestone-8/component_2/src/App.js
@@ -6,12 +6,14 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Counter></Counter> */}
+      <Counter></Counter>
       <ExternalUsers></ExternalUsers>
+      <LoadComments></LoadComments>
     </div>
   );
 }
 
+// load User data from API
 function ExternalUsers() {
   // to load data from API, firstly we have to setup a state
   const [users, setUsers] = useState([]); // if the API cannot be loaded for any reason, then the array's default value will be --> empty array
@@ -24,11 +26,32 @@ function ExternalUsers() {
     <div>
       <h3>External Users</h3>
       {
-        users.map(user => <User name={user.name} email={user.email}></User>)
+        users.map(user => <User name={user.name} email={user.email}></User>) // User --> has a Component below
       }
     </div>
   )
 }
+
+// load user Comments
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setComments(data))
+  }, []);
+
+  return (
+    <div>
+      <h3>Load Comments</h3>
+      {
+        comments.map(comment => <Comment title={comment.title} body={comment.body}> </Comment>) // Comment --> has a Component below
+      }
+    </div>
+  )
+}
+
+// ----------- Components ------------
 
 function User(props) {
   return (
@@ -39,8 +62,18 @@ function User(props) {
   )
 }
 
+function Comment(props) {
+  return (
+    <div className="product">
+      <h4>Title: {props.title}</h4>
+      <p>Body: {props.body}</p>
+    </div>
+  )
+}
+
+// useState & onClick Event Handler
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); // default count value = 0
   const handleIncrese = () => setCount(count + 1);
   const handleDecrese = () => setCount(count - 1);
 
@@ -48,6 +81,7 @@ function Counter() {
   return (
     <div>
       <h1>Count: {count}</h1>
+      {/* onClick --> C capital letter & have to use dynamic value using {} */}
       <button onClick={handleIncrese}>Increase</button>
       <button onClick={handleDecrese}>Decrease</button>
     </div>
