2b93ebd15c1d375c4264e28d22921020b61428b2
mdislammazharul <mazharul.py@gmail.com>
2021-09-21 19:40:20 +0600

    react countries

---
+++ Milestone-8/react_rest_countries/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Countries from './components/Countries/Countries';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Countries></Countries>
+    </div>
+  );
+}
+
+// function Countries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://restcountries.eu/rest/v2/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }, [])
+//   return (
+//     <div>
+//       <h2>Travelling around the world!</h2>
+//       <h4>Countries Available: {countries.length}</h4>
+//       {
+//         countries.map(country => <Country name={country.name}></Countries>)
+//       }
+//     </div>
+//   )
+// }
+export default App;
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//     </div>
+//   )
+// }
--- Milestone-8/work_project/src/App.js
+++ Milestone-8/work_project/src/App.js
@@ -1,38 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Countries from './components/Countries/Countries';
-
 
 function App() {
   return (
     <div className="App">
-      <Countries></Countries>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-// function Countries() {
-//   const [countries, setCountries] = useState([]);
-//   useEffect(() => {
-//     fetch('https://restcountries.eu/rest/v2/all')
-//       .then(res => res.json())
-//       .then(data => setCountries(data))
-//   }, [])
-//   return (
-//     <div>
-//       <h2>Travelling around the world!</h2>
-//       <h4>Countries Available: {countries.length}</h4>
-//       {
-//         countries.map(country => <Country name={country.name}></Countries>)
-//       }
-//     </div>
-//   )
-// }
 export default App;
-
-// function Country(props) {
-//   return (
-//     <div>
-//       <h2>Name: {props.name}</h2>
-//     </div>
-//   )
-// }
