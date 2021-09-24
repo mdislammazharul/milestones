1b45656b4df052c053201f0dea8375307b247323
mdislammazharul <mazharul.py@gmail.com>
2021-09-24 12:36:25 +0600

    ema-jhon-shopping

--- Milestone-8/ema_jhon_simple/src/components/Product/Product.js
+++ Milestone-8/ema_jhon_simple/src/components/Product/Product.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
+import Rating from 'react-rating';
 
 
 const Product = (props) => {
     // console.log(props);
-    const { name, price, seller, img, stock } = props.product;
+    const { name, price, seller, img, stock, star } = props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
 
     return (
@@ -19,6 +20,12 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p>Price: {price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="far fa-star icon-color"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly></Rating>
+                <br />
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
                     className="btn-regular">{cartIcon} add to cart</button>
