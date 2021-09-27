2f8687b4b270b073a5eed95673a5de286bc255d8
mdislammazharul <mazharul.py@gmail.com>
2021-09-27 23:18:35 +0600

    bootstrap

--- Milestone-8/work_project/src/App.js
+++ Milestone-8/work_project/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const items = [
+    { name: 'first item', description: 'Our Sahid Minar', img: 'https://media-eng.dhakatribune.com/uploads/2020/02/shaheed-minar-1582212737266.jpg' },
+    { name: 'first item', description: 'Our Sahid Minar', img: 'https://media-eng.dhakatribune.com/uploads/2020/02/shaheed-minar-1582212737266.jpg' },
+    { name: 'first item', description: 'Our Sahid Minar', img: 'https://media-eng.dhakatribune.com/uploads/2020/02/shaheed-minar-1582212737266.jpg' },
+    { name: 'first item', description: 'Our Sahid Minar', img: 'https://media-eng.dhakatribune.com/uploads/2020/02/shaheed-minar-1582212737266.jpg' },
+    { name: 'first item', description: 'Our Sahid Minar', img: 'https://media-eng.dhakatribune.com/uploads/2020/02/shaheed-minar-1582212737266.jpg' }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row row-cols-1 row-cols-md-md-4 g-4">
+      {
+        items.map(item => <Card item={item}>
+        </Card>)
+      }
     </div>
   );
 }
