489678b758b74fa3a3ad458c7cadf234b11c8cdd
mdislammazharul <mazharul.py@gmail.com>
2021-09-28 18:19:55 +0600

    animation

--- Milestone-9/work_project/src/App.js
+++ Milestone-9/work_project/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import { LineChart } from 'recharts';
+import { useSpring, animated } from 'react-spring'
+import { useState } from 'react';
 
 function App() {
+  const [flip, set] = useState(false)
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: () => set(!flip),
+  })
   return (
     <div className="App">
-      <LineChart></LineChart>
+      <animated.h1 style={props}>hello</animated.h1>
     </div>
   );
 }
