1a2b2ddb777f1a1e4b2f8d0d27e74ca557f03b2d
mdislammazharul <mazharul.py@gmail.com>
2021-09-29 08:40:03 +0600

    dynamic user url load

--- Milestone-9/work_project/src/components/FriendDetail/FriendDetail.js
+++ Milestone-9/work_project/src/components/FriendDetail/FriendDetail.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 const FriendDetail = () => {
+    const { friendId } = useParams();
+    const [friend, setFriend] = useState({})
+    useEffect(() => {
+        const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setFriend(data))
+    }, [])
     return (
         <div>
-            <h3>Detail Coming Soon</h3>
+            <h1>Name: {friend.name}</h1>
+            <h4>Phone: {friend.phone}</h4>
+            <h4>Website: {friend.website}</h4>
+            <p>Company: {friend?.company?.name}</p>
         </div>
     );
 };
