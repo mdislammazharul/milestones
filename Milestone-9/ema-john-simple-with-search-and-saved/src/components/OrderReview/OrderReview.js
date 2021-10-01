5186d4fb3b0eae2184a8782f40b4c5f6d5a6f70a
mdislammazharul <mazharul.py@gmail.com>
2021-10-01 12:09:33 +0600

    using hooks

---
+++ Milestone-9/ema-john-simple-with-search-and-saved/src/components/OrderReview/OrderReview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
+
+const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart] = useCart(products);
+    return (
+        <div>
+            <h1>{products.length}</h1>
+            <h3>{cart.length}</h3>
+            <h2>This is Order Review</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
