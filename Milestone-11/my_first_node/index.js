c2905d618a6c3a9eac3c2aa3fe5d6b5ee462cf17
mdislammazharul <mazharul.py@gmail.com>
2021-10-21 00:18:30 +0600

    node

---
+++ Milestone-11/my_first_node/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const users = [
+    { id: 0, name: 'Shabana', email: 'Shabana@gmail.com', phone: '01788888888' },
+    { id: 1, name: 'Shabnoor', email: 'Shabnoor@gmail.com', phone: '01788888888' },
+    { id: 2, name: 'Shrabonti', email: 'Shrabonti@gmail.com', phone: '01788888888' },
+    { id: 3, name: 'Suchorita', email: 'Suchorita@gmail.com', phone: '01788888888' },
+    { id: 4, name: 'Soniya', email: 'Soniya@gmail.com', phone: '01788888888' },
+    { id: 5, name: 'Susmita', email: 'Susmita@gmail.com', phone: '01788888888' },
+]
+
+// res- rsponse, req-request
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users[id];
+    res.send(user);
+});
+
+app.listen(port, () => {
+    console.log('listening to port', port);
+});
